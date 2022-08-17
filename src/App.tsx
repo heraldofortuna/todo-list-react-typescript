5d27564b203ff892a431c5c53e1ec110e5c0b50e
Heraldo Fortuna <heraldofortuna@gmail.com>
2022-08-17 11:38:18 -0500

    Se creo el estado 'Status', para renderizar un texto alternativo cuando está cargando la data inicial

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import * as React from "react";
 import api from "./Item/api";
 import { Item } from "./Item/types";
 
+enum Status {
+  Init = "init",
+  Success = "success",
+}
+
 const App: React.FC = () => {
   const [items, setItems] = React.useState<Item[]>([]);
+  const [status, setStatus] = React.useState<Status>(Status.Init);
 
   React.useEffect(() => {
     api.list().then((items) => {
       setItems(items);
+      setStatus(Status.Success);
     });
   }, []);
+
+  if (status == Status.Init) {
+    return <span>Loading ...</span>;
+  }
 
   return (
     <main>
